a5e2fb33a053a0863ced5db1a707967c819bce3a
Johannes Hirt <johanneshirt@Johanness-MacBook-Air-2.local>
2023-10-22 11:46:58 +0200

    inital commit

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+        public void test_method() {
+            System.out.println("Test method");
+        }
+}
